cb0569b517fc3adf5db9d0053b4bf1d1b12df2a4
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-07-27 18:15:25 +0600

    product api add and work running

--- lib/page/show_page/classicit_product_page.dart
+++ lib/page/show_page/classicit_product_page.dart
@@ -20,7 +20,7 @@ class _ProductState extends State<Product> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Products Page"),
+        title: const Text("Products Page"),
       ),
 
     );
